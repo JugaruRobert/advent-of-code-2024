cf8d3966b7f9e863e25703f54da9dea73c0dd3a7
Jugaru Robert <robertgeorgejugaru@gmail.com>
2024-12-03 23:49:37 +0200

    Day 3

---
+++ src/days/3/puzzle.ts
@@ -0,0 +1,43 @@
+import Puzzle from '../../types/abstract-puzzle';
+import { PuzzleResult } from '../../types/puzzle.types';
+
+export default class ConcretePuzzle extends Puzzle {
+  public solveFirst(): PuzzleResult {
+    const regex = /mul\(\d+,\d+\)/g;
+    const groupMatches = this.input.match(regex);
+
+    let result = 0;
+    groupMatches.forEach((groupMatch) => {
+      const numbers = groupMatch.match(/\d+/g).map((number) => Number(number));
+      result += numbers[0] * numbers[1];
+    });
+    return result;
+  }
+
+  public solveSecond(): PuzzleResult {
+    const regex = /mul\(\d+,\d+\)|do\(\)|don't\(\)/g;
+    const matches = [...this.input.matchAll(regex)].map((array) => array[0]);
+
+    let result = 0;
+    let isValid = true;
+
+    for (const match of matches) {
+      if (match === 'do()') {
+        isValid = true;
+        continue;
+      }
+
+      if (match === "don't()") {
+        isValid = false;
+        continue;
+      }
+
+      if (isValid) {
+        const numbers = match.match(/\d+/g).map((number) => Number(number));
+        result += numbers[0] * numbers[1];
+      }
+    }
+
+    return result;
+  }
+}
